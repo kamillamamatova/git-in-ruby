c7280f249df4f15cbc7ad4d41c8cc6807e942ecf
kamillamamatova <kamillamamatova.km@gmail.com>
2026-06-01 23:05:05 -0400

    Created a command dispatcher

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ I don't have ruby, so to install it on mac:
 brew install ruby
 
 and download a ruby extension on vscode
+
+It's also .rb
---
+++ bin/git-in-ruby.rb
@@ -0,0 +1,23 @@
+# Gets the first command line argument and stores the rest in args
+command, *args = ARGV
+
+# If the user didn't provide a command
+if command.nil?
+  # Prints a usage message and exits with a non 0 status code
+  $stderr.puts "Usage: git-in-ruby <command> [<args>]"
+  exit 1
+end
+
+# Builds the full path to the command executable
+path_to_command = File.expand_path("../git-in-ruby-#{command}", __FILE__)
+
+# Checks whether the command file actually exists
+if !File.exist? path_to_command
+  # Tells the user the command wasn't found
+  $stderr.puts "No such command"
+  exit 1
+end
+
+# Replaces the current Ruby process w/ the command executable
+# The *args operator expands the array into separate arguments
+exec path_to_command, *args
